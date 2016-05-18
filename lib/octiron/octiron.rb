09fe16cc13bd6167a2a3f219d105baea58007c14
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-05-18 16:21:16 +0200

    Octiron require also includes Octiron::World

---
+++ lib/octiron/octiron.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+#
+# octiron
+# https://github.com/jfinkhaeuser/octiron
+#
+# Copyright (c) 2016 Jens Finkhaeuser and other octiron contributors.
+# All rights reserved.
+#
+
+require 'octiron/version'
+require 'octiron/world'
