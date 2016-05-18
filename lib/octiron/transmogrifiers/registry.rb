c6326368a81fa0f124383f27338580a2b0e35aaa
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-05-18 14:01:51 +0200

    Basic transmogrifier registry implemented & specs added. What's missing is Hash prototype matching.

---
+++ lib/octiron/transmogrifiers/registry.rb
@@ -0,0 +1,173 @@
+# coding: utf-8
+#
+# octiron
+# https://github.com/jfinkhaeuser/octiron
+#
+# Copyright (c) 2016 Jens Finkhaeuser and other octiron contributors.
+# All rights reserved.
+#
+
+require 'rgl/adjacency'
+require 'rgl/dijkstra'
+
+require 'octiron/support/camel_case'
+require 'octiron/support/constantize'
+
+# require 'collapsium/recursive_sort'
+# require 'collapsium/prototype_match'
+
+module Octiron::Transmogrifiers
+
+  ##
+  # Registers transmogrifiers between one (event) class and another.
+  #
+  # A transmogrifier is an object with a call method or a block that accepts
+  # an instance of one (event) class and produces an instance of another
+  # (event) class.
+  #
+  # The registry also exposes a #transmogrify function which uses any
+  # registered transmogrifier, or raises an error if there is no
+  # transmogrification possible.
+  #
+  # One piece of magic makes this particularly powerful: the registry creates
+  # a graph of how to transmogrify an object to another by chaining
+  # transmogrifiers. That is, if there is a transmogrifier that turns A into B,
+  # and one that turns B into C, then by chaining both the registry can also
+  # turn A into C directly. (This is done via the 'rgl' gem).
+  class Registry
+    ##
+    # Ctor
+    def initialize
+      @graph = RGL::DirectedAdjacencyGraph.new
+      @visitor = RGL::DijkstraVisitor.new(@graph)
+      @map_data = {}
+      @map = nil
+      @transmogrifiers = {}
+    end
+
+    ##
+    # Register transmogrifier
+    #
+    # @param from (Class, String, other) A class or String nameing a
+    #     transmogrifier source. With prototype Hash matching, this would be
+    #     a prototype.
+    # @param to (Class, String, other) Transmogrifier target.
+    # @param overwrite (Boolean) The registry can only hold one transmogrifier
+    #     per from -> to pair. If overwrite is true, registering a
+    #     transmogrifier where one already exists overwrites the old one,
+    #     otherwise an exception is raised.
+    # @param transmogrifier_object (Object) Transmogrifier object that must
+    #     implement a `#call` method accepting an instance of the event class
+    #     provided in the first parameter. If nil, a block needs to be provided.
+    # @param transmogrifier_proc (Proc) Transmogrifier block that accepts an
+    #     instance of the event class provided in the first parameter. If nil, a
+    #     transmogrifier object must be provided.
+    def register(from, to, overwrite = false, transmogrifier_object = nil,
+                 &transmogrifier_proc)
+      transmogrifier = transmogrifier_proc || transmogrifier_object
+      if not transmogrifier
+        raise ArgumentError, "Please pass either an object or a transmogrifier "\
+            "block"
+      end
+
+      # Convert to canonical names
+      from_name = parse_name(from)
+      to_name = parse_name(to)
+      key = [from_name, to_name]
+
+      # We treat the graph as authoritative for what transmogrifiers exist.
+      if @graph.has_edge?(from_name, to_name)
+        if not overwrite
+          raise ArgumentError, "Registry already knows a transmogrifier for "\
+              "#{key}, aborting!"
+        end
+      end
+
+      # Add edges and map data for the shortest path search. We treat all paths
+      # as equally weighted.
+      @graph.add_edge(from_name, to_name)
+      @map_data[key] = 1
+      @map = RGL::EdgePropertiesMap.new(@map_data, true)
+
+      # Finally, register transmogrifier
+      @transmogrifiers[key] = transmogrifier
+    end
+
+    ##
+    # Deregister transmogrifier
+    #
+    # @param from (Class, String, other) A class or String nameing a
+    #     transmogrifier source. With prototype Hash matching, this would be
+    #     a prototype.
+    # @param to (Class, String, other) Transmogrifier target.
+    def deregister(from, to)
+      # Convert to canonical names
+      from_name = parse_name(from)
+      to_name = parse_name(to)
+      key = [from_name, to_name]
+
+      # Graph, map data and transmogrifiers need to be modified
+      @graph.remove_edge(from_name, to_name)
+      @map_data.delete(key)
+      @map = RGL::EdgePropertiesMap.new(@map_data, true)
+
+      @transmogrifiers.delete(key)
+    end
+
+    alias unregister deregister
+
+    ##
+    # Transmogrify an object of one class into another class.
+    def transmogrify(from, to)
+      # Get lookup keys
+      from_name = from.class.to_s
+      to_name = parse_name(to)
+
+      # We'll ask the graph for the shortest path. If there is none, we can't
+      # transmogrify. (Note: the @map changes with each registration/
+      # deregistration, so we instanciate the algorithm here).
+      algo = RGL::DijkstraAlgorithm.new(@graph, @map, @visitor)
+      path = algo.shortest_path(from_name, to_name)
+
+      if path.nil?
+        raise ArgumentError, "No transmogrifiers for #{[from_name, to_name]} "\
+            "found, aborting!"
+      end
+
+      # Transmogrify for each part of the path
+      input = from
+      result = nil
+      path.inject do |step_from, step_to|
+        key = [step_from, step_to]
+        result = @transmogrifiers[key].call(input)
+        if result.class.to_s != step_to
+          raise "Transmogrifier returned result of invalid class "\
+              "#{result.class}, aborting!"
+        end
+        input = result
+
+        # Make step_to the next step_from
+        next step_to
+      end
+      return result
+    end
+
+    private
+
+    include ::Octiron::Support::CamelCase
+    include ::Octiron::Support::Constantize
+
+    def parse_name(name)
+      case name
+      when Class
+        return name.to_s
+      when Hash
+        return name
+      when String
+        return constantize(name).to_s
+      else
+        return constantize("#{@default_namespace}::#{camel_case(name)}").to_s
+      end
+    end
+  end # class Registry
+end # module Octiron::Transmogrifiers
---
+++ spec/transmogrifiers_registry_spec.rb
@@ -0,0 +1,188 @@
+require 'spec_helper'
+require_relative '../lib/octiron/transmogrifiers/registry'
+
+class Test1
+end
+
+class Test2
+end
+
+class Test3
+end
+
+class Transmogrifier
+  attr_reader :invoked
+  def initialize
+    @invoked = 0
+  end
+
+  def call(_)
+    @invoked += 1
+  end
+end
+
+class Test1To2Transmogrifier < Transmogrifier
+  def call(from)
+    super
+    return Test2.new
+  end
+end
+
+class Test2To3Transmogrifier < Transmogrifier
+  def call(from)
+    super
+    return Test3.new
+  end
+end
+
+class Test1To3Transmogrifier < Transmogrifier
+  def call(from)
+    super
+    return Test3.new
+  end
+end
+
+describe Octiron::Transmogrifiers::Registry do
+  describe "construction" do
+    it "can be constructed" do
+      expect do
+        ::Octiron::Transmogrifiers::Registry.new
+      end.not_to raise_error
+    end
+  end
+
+  describe "registration" do
+    before :each do
+      @reg = ::Octiron::Transmogrifiers::Registry.new
+    end
+
+    it "can register a transmogrifier object" do
+      expect do
+        @reg.register(Test1, Test2, false, Transmogrifier.new)
+      end.not_to raise_error
+    end
+
+    it "throws when registering a transmogrifier for the same pair twice" do
+      expect do
+        @reg.register(Test1, Test2, false, Transmogrifier.new)
+      end.not_to raise_error
+
+      expect do
+        @reg.register(Test1, Test2, false, Transmogrifier.new)
+      end.to raise_error(ArgumentError)
+    end
+
+    it "can overwrite transmogrifiers" do
+      expect do
+        @reg.register(Test1, Test2, false, Transmogrifier.new)
+      end.not_to raise_error
+
+      expect do
+        @reg.register(Test1, Test2, true, Transmogrifier.new)
+      end.not_to raise_error
+    end
+
+    it "can register transmogrifier procs" do
+      expect do
+        @reg.register(Test1, Test2) do |_|
+        end
+      end.not_to raise_error
+    end
+
+    it "raises if no transmogrifier is given" do
+      expect do
+        @reg.register(Test1, Test2)
+      end.to raise_error(ArgumentError)
+
+      expect do
+        @reg.register(Test1, Test2, false)
+      end.to raise_error(ArgumentError)
+    end
+
+    it "can deregister a transmogrifier" do
+      expect do
+        @reg.register(Test1, Test2) do |_|
+        end
+      end.not_to raise_error
+
+      # First time, there is a transmogrifier
+      expect do
+        @reg.deregister(Test1, Test2)
+      end.not_to raise_error
+
+      # Second time, there is none - it still shouldn't fail
+      expect do
+        @reg.deregister(Test1, Test2)
+      end.not_to raise_error
+    end
+  end
+
+  describe "transmogrification" do
+    before :each do
+      @reg = ::Octiron::Transmogrifiers::Registry.new
+      @trans1to2 = Test1To2Transmogrifier.new
+      @trans2to3 = Test2To3Transmogrifier.new
+      @reg.register(Test1, Test2, false, @trans1to2)
+      @reg.register(Test2, Test3, false, @trans2to3)
+    end
+
+    it "can transmogrify with a directly registered transmogrifier" do
+      result = nil
+
+      expect do
+        result = @reg.transmogrify(Test1.new, Test2)
+      end.not_to raise_error
+
+      expect(result.class).to eql Test2
+
+      expect(@trans1to2.invoked).to eql 1
+      expect(@trans2to3.invoked).to eql 0
+    end
+
+    it "can transmogrify with indirectly registered transmogrifiers" do
+      result = nil
+
+      expect do
+        result = @reg.transmogrify(Test1.new, Test3)
+      end.not_to raise_error
+
+      expect(result.class).to eql Test3
+
+      expect(@trans1to2.invoked).to eql 1
+      expect(@trans2to3.invoked).to eql 1
+    end
+
+    it "chooses the shortest transmogrification path" do
+      result = nil
+
+      direct = Test1To3Transmogrifier.new
+      @reg.register(Test1, Test3, false, direct)
+
+      expect do
+        result = @reg.transmogrify(Test1.new, Test3)
+      end.not_to raise_error
+
+      expect(result.class).to eql Test3
+
+      expect(@trans1to2.invoked).to eql 0
+      expect(@trans2to3.invoked).to eql 0
+      expect(direct.invoked).to eql 1
+    end
+
+    it "fails if no transmogrifier is found" do
+      expect do
+        @reg.transmogrify(Test1.new, Hash)
+      end.to raise_error(ArgumentError)
+    end
+
+    it "fails if a transmogrifier misbehaves" do
+      # Register the wrong 'direct' transmogrifier that will then be chosen
+      direct = Test1To2Transmogrifier.new
+      @reg.register(Test1, Test3, false, direct)
+
+      expect do
+        @reg.transmogrify(Test1.new, Test3)
+      end.to raise_error(RuntimeError)
+    end
+  end
+end
