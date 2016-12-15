a0323c36831788a39b43bd84b06cf55032cb54fd
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-12-15 12:22:57 +0100

    Add event handler classes

--- lib/octiron/events/bus.rb
+++ lib/octiron/events/bus.rb
@@ -21,6 +21,9 @@ module Octiron::Events
     # @return (String) the default namespace to search for events
     attr_reader :default_namespace
 
+    # The default handler class
+    DEFAULT_CLASS = 'octiron-default'.freeze
+
     ##
     # @param default_namespace (Symbol) The default namespace to look in for
     #     Event classes.
@@ -46,10 +49,12 @@ module Octiron::Events
     #     event class provided in the first parameter. If nil, a handler object
     #     must be provided.
     # @return The class represented by the event_id, as a String name.
-    def subscribe(event_id, handler_object = nil, &handler_proc)
-      return with_handlers(event_id, handler_object, handler_proc) do |hlist, h|
-        hlist << h
-      end
+    def subscribe(event_id, handler_object = nil, handler_class = DEFAULT_CLASS,
+                  &handler_proc)
+      return with_handlers(event_id, handler_class, handler_object,
+                           handler_proc) do |hlist, h|
+               hlist << h
+             end
     end
     alias register subscribe
 
@@ -64,10 +69,12 @@ module Octiron::Events
     #     event class provided in the first parameter. If nil, a handler object
     #     must be provided.
     # @return The class represented by the event_id, as a String name.
-    def unsubscribe(event_id, handler_object = nil, &handler_proc)
-      return with_handlers(event_id, handler_object, handler_proc) do |hlist, h|
-        hlist.delete(h)
-      end
+    def unsubscribe(event_id, handler_object = nil, handler_class = DEFAULT_CLASS,
+                    &handler_proc)
+      return with_handlers(event_id, handler_class, handler_object,
+                           handler_proc) do |hlist, h|
+               hlist.delete(h)
+             end
     end
 
     ##
@@ -79,8 +86,10 @@ module Octiron::Events
       if not event.is_a?(Hash)
         event_name = event.class.to_s
       end
-      handlers_for(event_name, false).each do |handler|
-        handler.call(event)
+      @handlers.keys.sort.each do |handler_class|
+        handlers_for(event_name, handler_class, false).each do |handler|
+          handler.call(event)
+        end
       end
     end
     alias broadcast publish
@@ -91,7 +100,10 @@ module Octiron::Events
     # The first parameters is the event class or event name. The second parameter
     # is whether this is for write or read access. We don't want to pollute
     # @handlers with data from the #publish call.
-    def handlers_for(name, write)
+    def handlers_for(name, handler_class, write)
+      # Defaults for the class
+      @handlers[handler_class] ||= {}
+
       # Use prototype matching for Hash names
       if name.is_a?(Hash)
         name.extend(::Collapsium::PrototypeMatch)
@@ -106,7 +118,7 @@ module Octiron::Events
           best_proto = nil
 
           # Find the best matching prototype
-          @handlers.keys.each do |proto|
+          @handlers[handler_class].keys.each do |proto|
             score = name.prototype_match_score(proto)
             if score > best_score
               best_score = score
@@ -115,14 +127,14 @@ module Octiron::Events
           end
 
           if not best_proto.nil?
-            return @handlers[best_proto]
+            return @handlers[handler_class][best_proto]
           end
         else
           merged = []
 
-          @handlers.keys.each do |proto|
+          @handlers[handler_class].keys.each do |proto|
             if name.prototype_match(proto)
-              merged += @handlers[proto]
+              merged += @handlers[handler_class][proto]
             end
           end
 
@@ -140,19 +152,19 @@ module Octiron::Events
       # If we're in write access, make sure to store an empty list as well as
       # returning one (if necessary).
       if write
-        @handlers[name] ||= []
+        @handlers[handler_class][name] ||= []
       end
 
       # In read access, want to either return an empty list, or the registered
       # handlers, but not ovewrite the registered handlers.
-      return @handlers[name] || []
+      return @handlers[handler_class][name] || []
     end
 
-    def with_handlers(event_id, handler_object, handler_proc)
+    def with_handlers(event_id, handler_class, handler_object, handler_proc)
       handler = resolve_handler(handler_object, &handler_proc)
       event_name = identify(event_id)
 
-      yield handlers_for(event_name, true), handler
+      yield handlers_for(event_name, handler_class, true), handler
 
       return event_name
     end
